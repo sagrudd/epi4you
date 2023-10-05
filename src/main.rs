35344326771e90a64dfda4f0e4b520d20c367786
Stephen Rudd <stephen@mnemosyne.co.uk>
2023-10-05 11:14:10 +0200

    simple cmdline parse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use clap::{Parser, Subcommand};
+
+/// Trivial application to package EPI2ME workflows and analysis results
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Datatypes>,
+
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: Option<String>,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+#[derive(Subcommand)]
+enum Datatypes {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+
+    /// bioinformatics workflows
+    BfxFlows {
+
+    },
+
+    /// EPI2ME analysis results
+    EpiRes {
+
+    },
+}
+
 fn main() {
+    let _args = Args::parse();
     println!("Hello, world!");
 }
