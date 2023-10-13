c644067cdfd06bd6e0360fa160f0a5eca4ea26ee
Stephen Rudd <stephen@mnemosyne.co.uk>
2023-10-13 18:23:28 +0200

    first provenance thoughts

--- src/manifest.rs
+++ src/manifest.rs
@@ -2,37 +2,49 @@ use std::path::PathBuf;
 
 use crate::provenance::Epi2MeProvenance;
 
+static MANIFEST_JSON: &str = "4u_manifest.json";
 
-
-struct FileManifest {
-    filename: String,
-    relative_path: String,
-    size: String,
-    md5sum: String,
+pub struct FileManifest {
+    pub filename: String,
+    pub relative_path: String,
+    pub size: String,
+    pub md5sum: String,
 }
+impl Default for FileManifest {
+    fn default() -> FileManifest {
+
+        FileManifest {
+            filename: String::from("undefined"),
+            relative_path: String::from("undefined"),
+            size: String::from("undefined"),
+            md5sum: String::from("undefined"),
+        }
+    }
+}
+
 
 pub struct Epi2MeManifest {
-    id: String,
-    src_path: String,
-    provenance: Vec<Epi2MeProvenance>,
+    pub id: String,
+    pub src_path: String,
+    pub provenance: Vec<Epi2MeProvenance>,
 
-    payload: Vec<FileManifest>,
-    filecount: u8,
-    files_size: u8,
+    pub payload: Vec<FileManifest>,
+    pub filecount: u8,
+    pub files_size: u8,
 
-    signature: String,
+    pub signature: String,
 }
 
 
-pub fn manifest_exists() -> bool {
-    return false;
-}
 
 pub fn get_manifest(source: Option<PathBuf>) -> Option<Epi2MeManifest> {
     if source.is_some() {
-
-        if !manifest_exists() {
-
+        let mut manifest = source.clone().unwrap();
+        manifest.push(MANIFEST_JSON);
+        if !manifest.exists() {
+            // we need to create one
+        } else {
+            // we should load the manifest
         }
     }
     return None;
--- src/provenance.rs
+++ src/provenance.rs
@@ -1,11 +1,22 @@
-
-
+use uuid::Uuid;
+use chrono::prelude::*;
 
 pub struct Epi2MeProvenance {
-    id: String,
-
+    pub id: String,
+    pub action: String,
+    pub timestamp: DateTime<Local>,
 }
 
+impl Default for Epi2MeProvenance {
+    fn default() -> Epi2MeProvenance {
+
+        Epi2MeProvenance {
+            id: Uuid::new_v4().to_string(),
+            action: String::from("undefined"),
+            timestamp: Local::now(),
+        }
+    }
+}
 
 pub fn is_trusted() -> bool {
     return false;
