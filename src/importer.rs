538a182a1078d62988e9d73c049915eebfc20ba1
Stephen Rudd <stephen@mnemosyne.co.uk>
2024-01-15 09:55:10 +0100

    created distinct importer module; cleanup

---
+++ src/importer.rs
@@ -0,0 +1,64 @@
+use std::{path::PathBuf, collections::HashMap};
+
+use crate::{manifest::{load_manifest_from_tarball, Epi2MeContent, is_manifest_honest, import_resolved_content}, tempdir::{TempDir, get_named_tempdir}};
+
+
+
+pub fn import_manager(twome: &Option<String>, force: &bool) {
+
+    if twome.is_none() {
+        eprintln!("EPI2ME twome import requires a --twome <file> target to read");
+        return; 
+    } else {
+        let path = PathBuf::from(twome.as_ref().unwrap());
+        let manifest = load_manifest_from_tarball(&path);
+
+        if manifest.is_some() {
+
+            let mut content: HashMap<String, TempDir> = HashMap::new();
+
+            let payload = &manifest.as_ref().unwrap().payload;
+            for paypay in payload {
+                
+                match paypay {
+                    Epi2MeContent::Epi2meWf(epi2me_workflow) => {
+                        let files = &epi2me_workflow.files;
+                        for file in files {
+                            if !content.contains_key(&file.relative_path) {
+                                content.insert(file.relative_path.to_owned(), get_named_tempdir(&file.relative_path).unwrap());
+                            }
+                        }
+                    },
+                    
+                    Epi2MeContent::Epi2mePayload(desktop_analysis) => {
+                        let files = &desktop_analysis.files;
+                        for file in files {
+                            if !content.contains_key(&file.relative_path) {
+                                content.insert(file.relative_path.to_owned(), get_named_tempdir(&file.relative_path).unwrap());
+                            }
+                        }
+                    },
+                    
+                }
+
+            }
+
+            let honest = is_manifest_honest(&manifest.unwrap(), &path);
+            if honest.is_none() {
+                eprintln!("this epi4you archive is not trusted - exiting");
+                return;
+            } if honest.is_some() {
+                println!("importing something");
+                import_resolved_content(&honest.unwrap(), force);
+            }
+
+            
+
+        } else {
+            eprintln!("This archive may be malformed - cannot continue");
+        }
+    }
+    
+
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
-use std::{path::PathBuf, collections::HashMap};
-
 use app_db::dbmanager;
 use clap::{Parser, Subcommand, ArgAction};
 use docker::docker_agent;
 use epi2me_db::epi2me_manager;
-use manifest::{load_manifest_from_tarball, Epi2MeContent};
-use tempdir::{TempDir, get_named_tempdir};
+use importer::import_manager;
 use workflow::workflow_manager;
 
-mod epi2me_db;
-mod json;
+
 mod app_db;
-mod nextflow;
 mod bundle;
-mod manifest;
-mod provenance;
-mod workflow;
+mod epi2me_db;
 mod epi2me_tar;
 mod docker;
 mod dataframe;
+mod importer;
+mod json;
+mod manifest;
+mod nextflow;
+mod provenance;
 mod tempdir;
-
-
-use crate::manifest::{is_manifest_honest, import_resolved_content};
+mod workflow;
 
 /// Trivial application to package EPI2ME workflows and analysis results
 #[derive(Parser)]
@@ -216,61 +212,7 @@ async fn main() {
                 },
 
                 Some(Datatypes::Import { twome, force}) => {
-
-                    if twome.is_none() {
-                        eprintln!("EPI2ME twome import requires a --twome <file> target to read");
-                        return; 
-                    } else {
-                        let path = PathBuf::from(twome.as_ref().unwrap());
-                        let manifest = load_manifest_from_tarball(&path);
-
-                        if manifest.is_some() {
-
-                            let mut content: HashMap<String, TempDir> = HashMap::new();
-
-                            let payload = &manifest.as_ref().unwrap().payload;
-                            for paypay in payload {
-                                
-                                match paypay {
-                                    Epi2MeContent::Epi2meWf(epi2me_workflow) => {
-                                        let files = &epi2me_workflow.files;
-                                        for file in files {
-                                            if !content.contains_key(&file.relative_path) {
-                                                content.insert(file.relative_path.to_owned(), get_named_tempdir(&file.relative_path).unwrap());
-                                            }
-                                        }
-                                    },
-                                    
-                                    Epi2MeContent::Epi2mePayload(desktop_analysis) => {
-                                        let files = &desktop_analysis.files;
-                                        for file in files {
-                                            if !content.contains_key(&file.relative_path) {
-                                                content.insert(file.relative_path.to_owned(), get_named_tempdir(&file.relative_path).unwrap());
-                                            }
-                                        }
-                                    },
-                                    
-                                }
-
-                            }
-
-                            let honest = is_manifest_honest(&manifest.unwrap(), &path);
-                            if honest.is_none() {
-                                eprintln!("this epi4you archive is not trusted - exiting");
-                                return;
-                            } if honest.is_some() {
-                                println!("importing something");
-                                import_resolved_content(&honest.unwrap(), force);
-                            }
-
-                            
-
-                        } else {
-                            eprintln!("This archive may be malformed - cannot continue");
-                        }
-                    }
-                    
-
+                    import_manager(twome, force);
                 },
 
                 None => {}
